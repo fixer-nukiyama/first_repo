ba87fb09f45f67bc9add4de859518a8ed1e14287
抜山 真 <nukiyama.makoto@fixer.co.jp>
2018-09-21 12:39:30 +0900

    AddNewFile02

---
+++ Linq.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nukiyama.makoto
+ * Date: 2018/09/21
+ * Time: 10:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+//using System.Configuration;
+//using System.Data;
+//using System.Data.SqlClient;
+using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace test
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			try {
+				Console.WriteLine("Input ...");
+				int? input = Convert.ToInt32(Console.ReadLine());
+			
+				if (new[] { 0, 1, 2 }.Contains(input ?? 0))
+				{
+					Console.WriteLine("Safe");
+				}
+				else
+				{
+					Console.WriteLine("Out");
+				}
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine("Input Error.[{0}]",ex.ToString());
+			}
+
+			Console.ReadKey(true);
+		}
+	}
+}
