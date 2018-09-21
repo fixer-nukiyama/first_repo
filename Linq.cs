7a203bb931ce21a5bb30d2978c7e7215ca7e8f8e
fixer-nukiyama <nukiyama.makoto@fixer.co.jp>
2018-09-21 16:51:20 +0900

    test up

--- Linq.cs
+++ Linq.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Created by SharpDevelop.
  * User: nukiyama.makoto
  * Date: 2018/09/21
@@ -7,9 +7,6 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-//using System.Configuration;
-//using System.Data;
-//using System.Data.SqlClient;
 using System.Linq;
 //using System.Text;
 //using System.Threading.Tasks;
